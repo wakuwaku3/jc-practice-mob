eb1afceffde731bf35f7774b1b996444e3b78062
TakushiYokoyama <takushi.yokoyama@jointcrew.co.jp>
2018-10-24 20:50:24 +0900

    モブプロで作った

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,61 @@
 import * as React from 'react';
 import './App.css';
 
-import logo from './logo.svg';
+interface State {
+  query: string;
+  result: Book[];
+}
+interface BooksInfo {
+  '@graph': Array<{ items: Book[] }>;
+}
+interface Book {
+  title: string;
+  'dc:creator': string;
+}
 
-class App extends React.Component {
+class App extends React.Component<{}, State> {
+  private handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const query = e.currentTarget.value;
+    if (!query) {
+      this.setState({ query, result: [] });
+      return;
+    }
+    const url = `https://ci.nii.ac.jp/books/opensearch/search?format=json&q=${query}`;
+    const res = await fetch(url);
+    if (res.ok) {
+      const text = (await res.json()) as BooksInfo;
+      if (!text['@graph'] || text['@graph'].length <= 0) {
+        this.setState({ query, result: [] });
+        return;
+      }
+      const { items } = text['@graph'][0];
+      this.setState({ query, result: items });
+      return;
+    }
+    this.setState({ query, result: [] });
+  };
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      query: '',
+      result: [],
+    };
+  }
   public render() {
+    const { query, result } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div>
+        <input value={query} onChange={this.handleChange} />
+        <br />
+        <table>
+          {result.map(value => (
+            <tr>
+              <td>{value.title}</td>
+              <td>{value['dc:creator']}</td>
+            </tr>
+          ))}
+        </table>
       </div>
     );
   }
